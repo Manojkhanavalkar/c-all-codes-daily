cf4524966d82cfe4ea208d92157547055cf32b72
Manoj <khanavalkarmanoj@gmail.com>
2023-01-20 22:13:02 +0530

    adding a file to uderstand char datatype

---
+++ my_name.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    char c1,c2,c3,c4,c5;
+    c1='M';
+    c2='A';
+    c3='N';
+    c4='O';
+    c5='J';
+    cout<<"\nMy name is "<<c1<<c2<<c3<<c4<<c5;
+}
