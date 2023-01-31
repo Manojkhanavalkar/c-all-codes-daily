8116f2b88e150233c547976abc30c88e1cd69953
Manoj <khanavalkarmanoj@gmail.com>
2023-01-31 11:36:56 +0530

    payment slip created

--- payment_slip.cpp
+++ payment_slip.cpp
@@ -16,13 +16,13 @@ int main()
      cout<<"\n--------------------------------------------------------------";
      cout<<"\n              PAY SLIP:JULY MONTH       DATE:14 JULY          ";
      cout<<"\n--------------------------------------------------------------";
-     cout<<"\n\n Travelling allowance:"<<TA<<"\tProfessional tax:         "<<pt;
-     cout<<"\n\n Day allowance:       "<<DA<<"\tProfessional fund:        "<<pf;
+     cout<<"\n\n Travelling allowance:"<<TA<<"\tProfessional tax: "<<pt;
+     cout<<"\n\n Day allowance:       "<<DA<<"\tProfessional fund: "<<pf;
      cout<<"\n\n Home rent allowance: "<<HRA;
      cout<<"\n\n Basic salary:        "<<basic_salary;
      cout<<"\n==============================================================";
-     cout<<"\nTotal salry:    "<<gross_salary<<"                           =";
-     cout<<"\nCredited salary:"<<final_sal<<"                              =";
+     cout<<"\nTotal salry:    "<<gross_salary;
+     cout<<"\nCredited salary:"<<final_sal;
      cout<<"\n==============================================================";
 
 }
